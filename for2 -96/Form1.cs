1f25b9d67085482dcf6ecb721273e6f8a04d8ed0
hilal <130781977+mshelovee@users.noreply.github.com>
2023-10-23 12:59:01 +0300

    Add files via upload

---
+++ for2 -96/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace for2__96
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+
+            for (int i = 25; i < 76; i++)
+            {
+              lbSayilar.Items.Add(i);
+            }
+
+        }
+        
+        
+    }
+}
